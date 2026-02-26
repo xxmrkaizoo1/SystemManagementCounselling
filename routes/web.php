7b1edbafdfbf3bde321b66e0f4915301ccb85552
xxmrkaizoo1 <firasumairisgood@gmail.com>
2026-02-26 10:19:21 +0800

    add breeze package

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-    return view('index');
-})->name('home');
+    return view('welcome');
+});
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/signup', function () {
-    return view('signup');
-})->name('signup');
+require __DIR__.'/auth.php';
