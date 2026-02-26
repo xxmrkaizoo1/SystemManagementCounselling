4f9bf25ccca0bbab46acffe084b80d2569471b2e
xxmrkaizoo1 <firasumairisgood@gmail.com>
2026-02-26 10:39:20 +0800

    fix  err  &   duplicate cursor

---
+++ app/Http/Requests/Auth/AuthController.php
@@ -0,0 +1,33 @@
+<!-- <?php
+
+
+// namespace App\Http\Controllers\Auth;
+
+// use App\Http\Controllers\Controller;
+// use App\Models\Role;
+// use App\Models\User;
+// use Illuminate\Http\RedirectResponse;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Auth;
+// use Illuminate\Validation\Rule;
+// use Illuminate\View\View;
+
+// class AuthController extends Controller
+// {
+//     public function showLogin(): View
+//     {
+//         return view('login');
+//     }
+//     public function showSignup(): View
+//     {
+//         return view('signup');
+//     }
+
+//     }
+
+
+
+
+
+
+?> -->
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,28 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+Route::get( '/signup', function () {
+    return view('signup');
+})->name('signup');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
