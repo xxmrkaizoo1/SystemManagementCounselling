d8ad6f81288c791b6ec7000e3fd11c15d065b926
xxmrkaizoo1 <firasumairisgood@gmail.com>
2026-04-05 18:40:51 +0800

    change view

--- resources/views/admin/create-counsellor.blade.php
+++ resources/views/admin/create-counsellor.blade.php
@@ -90,13 +90,13 @@
                         <div>
                             <label for="password"
                                 class="block text-sm font-medium text-slate-700 mb-1.5">Password</label>
-                            <input id="password" name="password" type="password"
+                            <input id="password" name="password" type="text"
                                 class="w-full rounded-xl border border-slate-300 px-3 py-2.5 text-sm outline-none focus:ring-2 focus:ring-sky-500/30 focus:border-sky-500 transition" />
                         </div>
                         <div>
                             <label for="password_confirmation"
                                 class="block text-sm font-medium text-slate-700 mb-1.5">Confirm Password</label>
-                            <input id="password_confirmation" name="password_confirmation" type="password"
+                            <input id="password_confirmation" name="password_confirmation" type="text"
                                 class="w-full rounded-xl border border-slate-300 px-3 py-2.5 text-sm outline-none focus:ring-2 focus:ring-sky-500/30 focus:border-sky-500 transition" />
                         </div>
                     </div>
