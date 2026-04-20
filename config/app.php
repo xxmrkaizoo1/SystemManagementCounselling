6d1a86f47c9bfc1b1c3a9bc01566cd5669bf13be
xxmrkaizoo1 <firasumairisgood@gmail.com>
2026-04-20 09:51:47 +0800

    fix vairable issues on config

--- config/app.php
+++ config/app.php
@@ -79,5 +79,7 @@ return [
     'maintenance' => [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
-    ],_____
+    ],
+
+
 ];
