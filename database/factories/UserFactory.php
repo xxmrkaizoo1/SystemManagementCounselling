b50ac09f30c9458e6fcb9f929b6fdf7af8f8e0ae
xxmrkaizoo1 <firasumairisgood@gmail.com>
2026-05-06 00:57:38 +0800

    fix: use explicit FakerFactory in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,43 +5,35 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as FakerFactory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected static ?string $password = null;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
-        $fullName = $this->faker->name();
+        $faker = FakerFactory::create();
+
+        $fullName = $faker->name();
 
         return [
             'name' => $fullName,
             'full_name' => $fullName,
-            'phone' => $this->faker->numerify('01########'),
-            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $faker->numerify('01########'),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'years' => $this->faker->randomElement(['1SVM SEM1', '1SVM SEM2', '2SVM SEM3', null]),
-            'programme' => $this->faker->randomElement(['IPD', 'ISK', 'MTK 1', null]),
+            'years' => $faker->randomElement(['1SVM SEM1', '1SVM SEM2', '2SVM SEM3', null]),
+            'programme' => $faker->randomElement(['IPD', 'ISK', 'MTK 1', null]),
             'profile_pic' => null,
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
